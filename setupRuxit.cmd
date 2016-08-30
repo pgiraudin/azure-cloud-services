d2a3008157f190a9439137f3c70dca75b3a4e8de
Martin Goodwell <martin.gutenbrunner@dynatrace.com>
2016-08-30 07:24:50 +0200

    renaming

--- setupRuxit.cmd
+++ setupRuxit.cmd
@@ -1,7 +1,7 @@
 @echo off
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\setupRuxitLog.txt" 2>&1
-PowerShell ".\Install.ps1 %TENANT% %TOKEN%" >> "%TEMP%\setupRuxitLog.txt" 2>&1
+PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\setupOneAgentLog.txt" 2>&1
+PowerShell ".\Install.ps1 %TENANT% %TOKEN%" >> "%TEMP%\setupOneAgentLog.txt" 2>&1
 
-%StartupLocalStorage%\ruxit-agent-installer-latest.exe /quiet
+%StartupLocalStorage%\oneagent-installer-latest.exe /quiet
 echo %StartupLocalStorage% > %userprofile%\1.txt
 EXIT /B 0
