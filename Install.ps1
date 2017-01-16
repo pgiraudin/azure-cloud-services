7e7852193f34acaaaa9d4a415f377766e1bcf7f6
Martin Goodwell <martin.gutenbrunner@dynatrace.com>
2017-01-16 11:01:28 +0100

    Agent download now based on API token

--- Install.ps1
+++ Install.ps1
@@ -2,12 +2,13 @@ $args >> output.txt
 
 $tenant = $args[0]
 $token = $args[1]
-$url = "https://$tenant.live.dynatrace.com/installer/agent/windows/exe/latest/$token"
+$url = "https://$tenant.live.dynatrace.com/api/v1/deployment/installer/agent/windows/default/latest"
 
 $storagedir = "$env:StartupLocalStorage"
 $file = "$storagedir\oneagent-installer-latest.exe"
 
 $webclient = New-Object System.Net.WebClient
+$webclient.Headers.Add("Authorization","Api-Token $token");
 $webclient.DownloadFile($url,$file)
 
 exit 0
