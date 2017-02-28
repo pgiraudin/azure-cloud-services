96107d451f3a905e89f6bbc7975a4f9718f3df76
Martin Goodwell <martin.gutenbrunner@dynatrace.com>
2017-02-28 11:10:37 +0100

    back to old token
    quickfix

--- Install.ps1
+++ Install.ps1
@@ -2,13 +2,12 @@ $args >> output.txt
 
 $tenant = $args[0]
 $token = $args[1]
-$url = "https://$tenant.live.dynatrace.com/api/v1/deployment/installer/agent/windows/default/latest"
+$url = "https://$tenant.live.dynatrace.com/installer/agent/windows/exe/latest/$token"
 
 $storagedir = "$env:StartupLocalStorage"
 $file = "$storagedir\oneagent-installer-latest.exe"
 
 $webclient = New-Object System.Net.WebClient
-$webclient.Headers.Add("Authorization","Api-Token $token");
 $webclient.DownloadFile($url,$file)
 
 exit 0
