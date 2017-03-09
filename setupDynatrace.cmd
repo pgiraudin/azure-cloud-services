8ef93677e78c7c29d21c1fe492acf4286f587445
Martin Goodwell <martin.gutenbrunner@dynatrace.com>
2017-03-09 08:51:12 +0100

    Update setupDynatrace.cmd

--- setupDynatrace.cmd
+++ setupDynatrace.cmd
@@ -2,6 +2,5 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\setupOneAgentLog.txt" 2>&1
 PowerShell ".\Install.ps1 %ENVIRONMENTID% %TOKEN%" %CONNECTIONPOINT% >> "%TEMP%\setupOneAgentLog.txt" 2>&1
 
-%StartupLocalStorage%\oneagent-installer-latest.exe /quiet
 echo %StartupLocalStorage% > %userprofile%\1.txt
 EXIT /B 0
